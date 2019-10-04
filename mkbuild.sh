672b2ff67eefa306f29384135e92b2dcd84efcdc
José Bollo <jose.bollo@iot.bzh>
2019-10-04 13:41:46 +0200

    Add facility to compile
    Change-Id: Ib4d4a03f79056ae17ba1112657eb7fe37b194527
    Signed-off-by: José Bollo <jose.bollo@iot.bzh>

---
+++ mkbuild.sh
@@ -0,0 +1,15 @@
+#/bin/sh
+
+set -e
+h=$(dirname $0)
+mkdir -p $h/build 
+cd $h/build
+[ "$1" = "-f" ] && rm -r *
+cmake \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_INSTALL_PREFIX=~/.local \
+	-DWITH_SYSTEMD=ON \
+	-DWITH_CYNARA_COMPAT=ON \
+	..
+make -j
+
