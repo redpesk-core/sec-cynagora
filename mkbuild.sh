13907dd9110838c674683e35739344a66a5d9223
José Bollo <jose.bollo@iot.bzh>
2021-01-26 13:50:48 +0100

    mkbuild: Improvement

--- mkbuild.sh
+++ mkbuild.sh
@@ -1,16 +1,33 @@
 #/bin/sh
 
-set -e
-h=$(dirname $0)
-mkdir -p $h/build 
-cd $h/build
-[ "$1" = "-f" ] && rm -r *
+h="$(dirname $0)"
+force=false
+: ${bd:=build}
+: ${PREFIX:=$HOME/.local}
+eval set -- $(getopt -o b:fp: -l buildir:,force,prefix: -- "$@") || exit
+while :; do
+	case "$1" in
+	-b|--buildir) bd="$2"; shift;;
+	-f|--force) force=true;;
+	-p|--prefix) PREFIX="$2"; shift;;
+	--) shift; break;;
+	esac
+	shift
+done
+
+mkdir -p "$h/$bd" || exit
+cd "$h/$bd" || exit
+
+$force && { rm -r * 2>/dev/null || rm CMakeCache.txt 2>/dev/null; }
+test -f CMakeCache.txt -a -f Makefile || \
 cmake \
-	-DCMAKE_BUILD_TYPE=Debug \
-	-DCMAKE_INSTALL_PREFIX=~/.local \
-	-DWITH_SYSTEMD=ON \
-	-DWITH_CYNARA_COMPAT=ON \
-	-DDIRECT_CYNARA_COMPAT=ON \
+	-DPROJECT_VERSION=9.0.0 \
+	-DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX:=$PREFIX} \
+	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE:=Debug} \
+	-DWITH_SYSTEMD=${WITH_SYSTEMD:=ON} \
+	-DWITH_CYNARA_COMPAT=${WITH_CYNARA_COMPAT:=OFF} \
+	-DDIRECT_CYNARA_COMPAT=${DIRECT_CYNARA_COMPAT:=OFF} \
 	..
-make -j
+
+make -j "$@"
 
