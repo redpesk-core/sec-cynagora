dfdd2fbf2ad11412fa602c8a852050dc8f5aaa0c
José Bollo <jose.bollo@iot.bzh>
2023-03-21 19:16:48 +0100

    socket: Fix abstract socket length

--- src/socket.c
+++ src/socket.c
@@ -20,6 +20,7 @@
 /******************************************************************************/
 /******************************************************************************/
 
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
@@ -119,14 +120,14 @@ static int open_unix(const char *spec, int server)
 		return -1;
 	}
 
-	/* remove the file on need */
-	if (server && !abstract)
-		unlink(spec);
-
 	/* create a  socket */
 	fd = socket(AF_UNIX, SOCK_STREAM, 0);
 	if (fd < 0)
 		return fd;
+
+	/* remove the file on need */
+	if (server && !abstract)
+		unlink(spec);
 
 	/* prepare address  */
 	memset(&addr, 0, sizeof addr);
@@ -135,10 +136,11 @@ static int open_unix(const char *spec, int server)
 	if (abstract)
 		addr.sun_path[0] = 0; /* implement abstract sockets */
 
+	length += offsetof(struct sockaddr_un, sun_path) + !abstract;
 	if (server) {
-		rc = bind(fd, (struct sockaddr *) &addr, (socklen_t)(sizeof addr));
+		rc = bind(fd, (struct sockaddr *) &addr, (socklen_t)length);
 	} else {
-		rc = connect(fd, (struct sockaddr *) &addr, (socklen_t)(sizeof addr));
+		rc = connect(fd, (struct sockaddr *) &addr, (socklen_t)length);
 	}
 	if (rc < 0) {
 		close(fd);
